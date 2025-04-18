0d9f121e2ea79bd7ac6ba23b70fbf2b41e49e6a2
Massil <160241880+Massil-br@users.noreply.github.com>
2025-04-18 10:42:01 +0200

    ignore exe

--- Server/EXE/server.ps1
+++
@@ -1,9 +0,0 @@
-# Ce script ouvre une nouvelle console PowerShell en admin et lance ton serveur
-$cheminServeur = Split-Path -Parent $MyInvocation.MyCommand.Path
-$executable = "Server.exe"
-
-Start-Process powershell -Verb RunAs -ArgumentList @(
-    "-NoExit",
-    "-Command",
-    "cd '$cheminServeur'; .\\$executable"
-)
